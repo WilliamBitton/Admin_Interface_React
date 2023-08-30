1cfbeb504113a24d587ec459bb63895f692259ce
WilliamBitton <118488828+WilliamBitton@users.noreply.github.com>
2023-08-30 14:12:27 -0400

    commit

--- src/components/organisms/product-form/product-form.tsx
+++ src/components/organisms/product-form/product-form.tsx
@@ -104,7 +104,6 @@ export default function ProductForm(props: ProductFormProps) {
       setAlertSeverity("error");
       setAlertMessage(t("alertError.error"));
       setAlertOpen(true);
-      setLoading(false);
     } finally {
       setLoading(false);
     }
